7d5c8ed744a3d47f2d1ba2ce635991895b805725
AmirHossein Najmi <amirnajmi1992@gmail.com>
2022-07-22 13:31:03 +0300

    Top level Function and variables tutorials. Project initialization.

---
+++ src/main/kotlin/session1/Variables.kt
@@ -0,0 +1,28 @@
+package session1
+
+/**
+ * EXPLICIT TYPE AND TYPE INFERENCE
+ * There are two ways for defining the variables in kotlin, first with type representation and second with inference type.
+ * */
+fun variableTypeSample(){
+
+    // explicit type definition
+    var explicitTypeVar: Int = 10
+
+    // type inference
+    var inferenceTypeVar = "string"
+
+}
+
+/** VAL VS. VAR
+ * When you create a variable with val keyword, you can not reassign a new value to that variable. it works same as final keyword for variables in Java
+ * var is a normal type of variable. it means you can reassign value to that vars.
+ * */
+fun valAndVar(){
+
+}
+
+
+fun valTips(){
+
+}
